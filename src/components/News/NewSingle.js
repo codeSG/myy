1aab57fb3283c7a53750fb81ba8b54801c5b3b15
Sourabh Garg <sogar@microsoft.com>
2019-07-20 12:52:10 +0530

    API updated

--- src/components/News/NewSingle.js
+++ src/components/News/NewSingle.js
@@ -2,17 +2,10 @@ import React from 'react';
 const NewSingle = ({item}) => (
 	<div>
 		<div className="col s12"> </div>
-		<div className="col s1 z-depth-2 blue lighten-4" > {item.Name} </div>
-		<div className="col s2 z-depth-2 blue lighten-4" > {item.EmailId} </div>
-		<div className="col s1 z-depth-2 blue lighten-4" > {item.Phone} </div>
-		<div className="col s1 z-depth-2 blue lighten-4" > {item.Degree} </div>
-		<div className="col s1 z-depth-2 blue lighten-4" > {item.University} </div>
-		<div className="col s1 z-depth-2 blue lighten-4" > {item.Company} </div>
-		<div className="col s1 z-depth-2 blue lighten-4" > {item.Experience} </div>
-		<div className="col s1 z-depth-2 blue lighten-4" > {item.StartTime} </div>
-		<div className="col s1 z-depth-2 blue lighten-4" > {item.State} </div>
-		<div className="col s1 z-depth-2 blue lighten-4" > {item.ResumeLink} </div>
-		<div className="col s1 z-depth-2 blue lighten-4" > {item.EventId} </div>
+		<div className="col s4 z-depth-2 blue lighten-4" > {item.title} </div>
+		<div className="col s4 z-depth-2 blue lighten-4" > {item.author} </div>
+		<div className="col s4 z-depth-2 blue lighten-4" > {item.publishedAt} </div>
+		
 		<div className="col s12"> </div>
 				
 	</div>
--- src/components/News/News.js
+++ src/components/News/News.js
@@ -11,18 +11,18 @@ class News extends Component{
 	}
 
 	componentDidMount(){
-		const url = "https://newsapi.org/v2/everything?q=bitcoin&from=2019-06-19&sortBy=publishedAt&apiKey=6835bf4d60e84905a8009886c1b15b84"
+		const url = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=6835bf4d60e84905a8009886c1b15b84"
 		const url2 = "http://www.mshire.com:5000/books"
 		const url3 = "https://localhost:5001/api/candidate/getallWithoutFeedBack";
 		var proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-		fetch(url2)
+		fetch(url)
 			.then((response) => {
 				return response.json();
 			})
 			.then((data) => {
 				console.log(data)
 				this.setState({
-					news : data.books
+					news : data.articles
 				})
 			})
 			.catch((error) => console.log(error));
@@ -31,7 +31,7 @@ class News extends Component{
 	renderItems() {
 
 		return this.state.news.map((item) => (
-			<NewSingle key={item.url} item = {item} />
+			<NewSingle key={item.title} item = {item} />
 			));
 	}
 
@@ -40,18 +40,10 @@ class News extends Component{
 			
 			<div className="row">
 				<div className="col s12"> </div>
-				<div className="col s1 z-depth-2 blue-grey lighten-4" > NAME </div>
-				<div className="col s2 blue-grey lighten-1" > EMAIL-ID </div>
-				<div className="col s1 blue-grey lighten-4" > PHONE </div>
-				<div className="col s1 blue-grey lighten-1" > DEGREE </div>
-				<div className="col s1 blue-grey lighten-4" > UNIVERSITY </div>
-				<div className="col s1 blue-grey lighten-1" >COMPANY </div>
-				<div className="col s1 blue-grey lighten-4" > EXPERIENCE </div>
-				<div className="col s1 blue-grey lighten-1" > START TIME </div>
-				<div className="col s1 blue-grey lighten-4" > STATE </div>
-				<div className="col s1 blue-grey lighten-1" > RESUME </div>
-				<div className="col s1 blue-grey lighten-4" > EVENT-ID</div>
-				<div className="col s12"> </div>
+				<div className="col s4 z-depth-2 blue-grey lighten-4" > NAME </div>
+				<div className="col s4 blue-grey lighten-1" > EMAIL-ID </div>
+				<div className="col s4 blue-grey lighten-4" > PHONE </div>
+				
 				{this.renderItems()}
 			</div>
 		);
